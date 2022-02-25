79491920f90156718861cc4e823e4061a2dc04c7
ikalemmon <nagatadaiki@MacBook-Pro-142.local>
2022-02-26 02:05:40 +0900

    2

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # root 'application#hello'
-  get 'movies' ,to:'movies#index'
+  get '/movies' ,to:'movies#index'
+  namespace :admin do
+    #resources :movies
+    get'/movies', to:'movies#index'
+  end
 end
