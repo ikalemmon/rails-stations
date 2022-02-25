034836a209834b3c2644721083bfcddf4d460182
ikalemmon <nagatadaiki@MacBook-Pro-142.local>
2022-02-26 03:56:18 +0900

    3-4

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,5 @@
 class Movie < ApplicationRecord
-    validates :name, presence: true#,uniqness:true
+    validates :name, presence: true,uniqueness:true
     validates :year, presence: true, length:{is:4},format:{with:/\A[0-9]+\z/}
     validates :description, presence: true
     validates :image_url, presence: true, format:/\A#{URI::regexp(%w(http https))}\z/
